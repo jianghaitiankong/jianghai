f31f32054ef7f5ac4df77a643675cc17db3cd250
qingjun huo <somnus@qingjundeMacBook-Pro.local>
2019-06-18 15:38:25 +0800

    elementUi改造

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,96 +1,141 @@
 <template>
-	<div class="about">
-		<towtop></towtop>
-		<div class="flexCenter">
-			<div class="title fadeInUp animated">
-					<h3>{{businessTitle}}</h3>
-					<p>{{businessTitleEng}}</p>
-			</div>
-		</div>
-		<div class="box1Top container">
-			<p>{{desc}}</p >
-		</div>
-		<towbottom></towbottom>
-	</div>
+  <div class="about">
+    <towtop></towtop>
+    <div class="flexCenter">
+      <div class="title fadeInUp animated">
+        <h3>{{businessTitle}}</h3>
+        <p>{{businessTitleEng}}</p>
+      </div>
+    </div>
+    <div class="box1Top container">
+      <el-radio v-model="radio" label="1">备选项</el-radio>
+      <el-radio v-model="radio" label="2">备选项</el-radio>
+      <p>{{desc}}</p>
+      <!-- <el-time-select v-model="value" :picker-options="{start: '08:30', step: '00:15', end: '18:30'}" placeholder="选择时间"></el-time-select> -->
+      <div class="block">
+        <el-slider v-model="value" show-input></el-slider>
+        <el-button plain @click="open1">可自动关闭</el-button>
+
+        <el-button plain @click="open1">成功</el-button>
+        <el-button plain @click="open2">警告</el-button>
+        <el-button plain @click="open3">消息</el-button>
+        <el-button plain @click="open4">错误</el-button>
+      </div>
+    </div>
+    <towbottom></towbottom>
+  </div>
 </template>
 
 <script>
-	import towtop from '@/components/testTop'
-	import towbottom from '@/components/testBottom'
-	export default {
-		data() {
-			return {
-				businessTitle: "此刻相识",
-        businessTitleEng: 'About Us',
-				headerBg: 'headerBg',
-				imgUrl:'../../static/images/logo.png',
-				desc:'',
-				token: sessionStorage.getItem("token"),
-			}
-			
-		},
-		created(){
-			this.getAbout()
-		},
-		components: {
-			towtop,
-			towbottom
-		},
-		methods:{
-			getAbout() {
-				var about = {}
-				this.$request.post("aboutMe/selectAboutUs", about, this.token).then(res => {
-					this.desc=res.data.list[0].aboutUs
-				}).catch(err => {
-					console.log(error);
-				});
-			},
-		}
-	}
+import towtop from "@/components/testTop";
+import towbottom from "@/components/testBottom";
+export default {
+  data() {
+    return {
+      businessTitle: "此刻相识",
+      businessTitleEng: "About Us",
+      headerBg: "headerBg",
+      imgUrl: "../../static/images/logo.png",
+      desc: "",
+      token: sessionStorage.getItem("token"),
+      radio: "1",
+      value: 0
+    };
+  },
+  created() {
+    this.getAbout();
+  },
+  components: {
+    towtop,
+    towbottom
+  },
+  methods: {
+    getAbout() {
+      var about = {};
+      this.$request
+        .post("aboutMe/selectAboutUs", about, this.token)
+        .then(res => {
+          this.desc = res.data.list[0].aboutUs;
+        })
+        .catch(err => {
+          console.log(error);
+        });
+    },
+    open1() {
+      this.$notify({
+        title: "成功",
+        message: "这是一条成功的提示消息",
+        type: "success"
+      });
+    },
+
+    open2() {
+      this.$notify({
+        title: "警告",
+        message: "这是一条警告的提示消息",
+        type: "warning"
+      });
+    },
+
+    open3() {
+      this.$notify.info({
+        title: "消息",
+        message: "这是一条消息的提示消息"
+      });
+    },
+
+    oopen4() {
+      this.$notify.error({
+        title: "错误",
+        message: "这是一条错误的提示消息"
+      });
+    }
+  }
+};
 </script>
 
 <style scoped>
-	.about{
-		width:100%;
-		background:url(../../static/images/background4.png);
-		background-size: cover;
-	}
-	.kbox1Bg {
-		box-sizing: border-box;
-		margin-top: 130px;
-		margin-bottom: 100px;
-		padding-top:40px;
-		display: flex;
-	    flex-direction: row;
-	    flex-wrap: nowrap;
-	}
-	.kbox1Bg p{
-		text-indent: 2em;
-		line-height: 29px;
-		writing-mode: vertical-lr;
-		writing-mode: tb-lr;
-		margin-left: 43px;
-		height: 250px;
-		margin-top:30px;
-	}
-	.kbox1Bg img{
-		width: 77px;
-		height: 175px;
-	}
-	.box1Top {
-		/* margin-top:80px; */
-		padding-top: 30px;
-		padding-bottom: 40px;
-		min-height: 760px;
-	}
-	.box1Top p {
-		/* padding: 0 80px; */
-		text-indent: 2em;
-		line-height: 29px;
-		/* writing-mode: vertical-lr; */
-		/* writing-mode: tb-lr; */
-		/* margin-left: 43px; */
-		height: 250px;
-		/* margin-top:30px; */
-	}
+.about {
+  width: 100%;
+  background: url(../../static/images/background4.png);
+  background-size: cover;
+}
+.kbox1Bg {
+  box-sizing: border-box;
+  margin-top: 130px;
+  margin-bottom: 100px;
+  padding-top: 40px;
+  display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+}
+.kbox1Bg p {
+  text-indent: 2em;
+  line-height: 29px;
+  writing-mode: vertical-lr;
+  writing-mode: tb-lr;
+  margin-left: 43px;
+  height: 250px;
+  margin-top: 30px;
+}
+.kbox1Bg img {
+  width: 77px;
+  height: 175px;
+}
+.box1Top {
+  /* margin-top:80px; */
+  padding-top: 30px;
+  padding-bottom: 40px;
+  min-height: 760px;
+}
+.box1Top p {
+  /* padding: 0 80px; */
+  text-indent: 2em;
+  line-height: 29px;
+  /* writing-mode: vertical-lr; */
+  /* writing-mode: tb-lr; */
+  /* margin-left: 43px; */
+  height: 250px;
+  /* margin-top:30px; */
+}
 </style>
--- src/pages/forgetPsw.vue
+++ src/pages/forgetPsw.vue
@@ -1,97 +1,130 @@
 <template>
   <div>
-		<towtop></towtop>
-		<div class="pswBox">
+    <towtop></towtop>
+    <div class="pswBox">
       <div id="app" class="outBox f-cf">
-        <div class="u-op ">
+        <div class="u-op">
           <div class="m-title">忘记密码</div>
-          <div class="m-op-box">    
-            <div class="m-vfAccount m-center">      
-              <div class="u-tip">请输入要重置密码的手机号</div>     
-              <div class="w-ipt">           
-                <div class="m-suggest" >
-                  <div class="u-ipt ">
+          <div class="m-op-box">
+            <div class="m-vfAccount m-center">
+              <div class="u-tip">请输入要重置密码的手机号</div>
+              <div class="w-ipt">
+                <div class="m-suggest">
+                  <div class="u-ipt">
                     <div class="ipt_wrap">
-                      <input type="text" @blur="phoneBlur" v-model="phone" spellcheck="false" name="" placeholder="手机号码" class="ipt_input ipt_input_large ipt_input-success" maxlength="11" autocomplete="off">
+                      <input
+                        type="text"
+                        @blur="phoneBlur"
+                        v-model="phone"
+                        spellcheck="false"
+                        name
+                        placeholder="手机号码"
+                        class="ipt_input ipt_input_large ipt_input-success"
+                        maxlength="11"
+                        autocomplete="off"
+                      >
                       <div class="ipt_extend">
                         <i class="i-icon i-icon-delete" style="display: none;"></i>
                       </div>
                     </div>
-                    
-                    <div class="ipt_tip tipFailed"  v-show="phoneShow">
-                      <!-- {{phoneCont}} -->
+
+                    <!-- <div class="ipt_tip tipFailed" v-show="phoneShow">
+                      {{phoneCont}}
                       请输入正确的手机号
-                    </div>
+                    </div> -->
                   </div>
-                </div>         
+                </div>
               </div>
               <!-- 短信验证码 -->
               <div class="u-tip">点击获取验证码，短信将发送至您的手机</div>
-              <div class="m-suggest">     
-                <div class="u-ipt ipt-sms">        
-                  <div class="u-ipt ">
+              <div class="m-suggest">
+                <div class="u-ipt ipt-sms">
+                  <div class="u-ipt">
                     <div class="ipt_wrap">
-                      <input type="text" v-model="code" spellcheck="false" name="" placeholder="输入短信验证码" class="ipt_input ipt_input_small ipt_input-" maxlength="6" autocomplete="off">
+                      <input
+                        type="text"
+                        v-model="code"
+                        spellcheck="false"
+                        name
+                        placeholder="输入短信验证码"
+                        class="ipt_input ipt_input_small ipt_input-"
+                        maxlength="6"
+                        autocomplete="off"
+                      >
                       <div class="ipt_extend">
                         <i class="i-icon i-icon-delete" style="display: none;"></i>
                       </div>
                     </div>
                     <div class="u-btn-sms">
-                      <button v-show="sendAuthCode" @click.stop='sendSms'>获取验证码</button>
-                      <button v-show="!sendAuthCode" type="button" >{{auth_time}}秒后重新发送验证码</button>
+                      <button v-show="sendAuthCode" @click.stop="sendSms">获取验证码</button>
+                      <button v-show="!sendAuthCode" type="button">{{auth_time}}秒后重新发送验证码</button>
                     </div>
-                    <div class="ipt_tip "  v-bind:class = "{tipSuccess:msgColor , tipFailed:!msgColor}" >{{codeMsg}}</div>
-                  </div>           
-                </div>                   
+                  </div>
+                </div>
               </div>
               <!-- 新密码 -->
-              <div class="u-tip">请输入新密码</div>     
-              <div class="w-ipt">           
+              <div class="u-tip">请输入新密码</div>
+              <div class="w-ipt">
                 <div class="m-suggest">
-                  <div class="u-ipt ">
+                  <div class="u-ipt">
                     <div class="ipt_wrap">
-                      <input type="password" @blur="pswBlur" v-model="password" spellcheck="false" name="" placeholder="填入新密码" class="ipt_input ipt_input_large ipt_input-success" maxlength="20" autocomplete="off">
+                      <input
+                        type="password"
+                        @blur="pswBlur"
+                        v-model="password"
+                        spellcheck="false"
+                        name
+                        placeholder="填入新密码"
+                        class="ipt_input ipt_input_large ipt_input-success"
+                        maxlength="20"
+                        autocomplete="off"
+                      >
                       <div class="ipt_extend">
                         <i class="i-icon i-icon-delete" style="display: none;"></i>
                       </div>
                     </div>
-                    
-                    <div class="ipt_tip tipFailed" v-show="passwordShow">
-                      密码必须含有字母和数字，且长度在8-20之间
-                    </div>
                   </div>
-                </div>         
+                </div>
               </div>
               <!-- 新密码 -->
-              <div class="u-tip">请再次输入新密码</div>     
-              <div class="w-ipt">           
+              <div class="u-tip">请再次输入新密码</div>
+              <div class="w-ipt">
                 <div class="m-suggest">
-                  <div class="u-ipt ">
+                  <div class="u-ipt">
                     <div class="ipt_wrap">
-                      <input type="password" @blur="psw1Blur" v-model="password1" spellcheck="false" name="" placeholder="再次填入新密码" class="ipt_input ipt_input_large ipt_input-success" maxlength="20" autocomplete="off">
+                      <input
+                        type="password"
+                        @blur="psw1Blur"
+                        v-model="password1"
+                        spellcheck="false"
+                        name
+                        placeholder="再次填入新密码"
+                        class="ipt_input ipt_input_large ipt_input-success"
+                        maxlength="20"
+                        autocomplete="off"
+                      >
                       <div class="ipt_extend">
                         <i class="i-icon i-icon-delete" style="display: none;"></i>
                       </div>
                     </div>
-                    <div class="ipt_tip tipFailed" v-show="password1Show">
-                      两次密码不匹配
-                    </div>
                   </div>
-                </div>         
+                </div>
               </div>
-              <div class="u-btn ">
-                <button @click="forgrtPsw" class="btn btn-primary" :disabled="btnDis" :class="{disStyle:btnDis,canStyle:!btnDis}">提交</button>
-              </div>    
-              <div v-show="SubmitShow" class="btnTip"  :class = "{tipSuccess:tipColor , tipFailed:!tipColor}">      
-                  {{SubmitMsg}} 
-              </div> 
+              <div class="u-btn">
+                <button
+                  @click="forgrtPsw"
+                  class="btn btn-primary"
+                  :disabled="btnDis"
+                  :class="{disStyle:btnDis,canStyle:!btnDis}"
+                >提交</button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-		</div>
-    
-		<towbottom></towbottom>
+    </div>
+
+    <towbottom></towbottom>
   </div>
 </template>
 <script>
@@ -101,22 +134,18 @@ export default {
   data() {
     return {
       phone: "",
-      code : "",
-      password : "",
+      code: "",
+      password: "",
       password1: "",
-      sendAuthCode:true,/* 布尔值，通过v-show控制显示‘获取按钮’还是‘倒计时’ */
-      auth_time: 0, /* 倒计时 计数器 */
-      codeMsg : '',
-      msgColor : false, // 短信提示字体颜色
-      phoneShow : false,
-      passwordShow : false,
-      password1Show : false,
-      SubmitShow : false,
-      SubmitMsg : "密码重置成功",
-      dialogVisible: false,
-      tipColor : false,// 提交提示字体颜色
-      btnDis : false,
+      sendAuthCode: true /* 布尔值，通过v-show控制显示‘获取按钮’还是‘倒计时’ */,
+      auth_time: 0 /* 倒计时 计数器 */,
+     
+      btnDis: false,
       token: sessionStorage.getItem("token"),
+      successMsg: "", //成功的提示
+      waringMsg: "", //警告的提示
+      Message: "", //普通的提示
+      errorMsg: "" //错误的提示
     };
   },
   created() {},
@@ -125,115 +154,152 @@ export default {
     towbottom
   },
   methods: {
-    phoneBlur:function(){
-      if(!(/^1[34578]\d{9}$/.test(this.phone))){
-        this.phoneShow = true;
-      }else {
-        this.phoneShow = false
-      }
+    phoneBlur: function() {
+      if (!/^1[34578]\d{9}$/.test(this.phone)) {
+        this.errorMsg = "手机号码不正确";
+        this.open4();
+      } 
+     
     },
-    pswBlur:function(){
-      if(!(/^(?=.*[a-z])(?=.*\d)[^]{8,20}$/.test(this.password))){
-        this.passwordShow = true;
-      }else {
-        this.passwordShow = false;
-      }
+    pswBlur: function() {
+      if (!/^(?=.*[a-z])(?=.*\d)[^]{8,20}$/.test(this.password)) {
+        this.errorMsg = "密码只能含有字母和数字，长度在8-20之间";
+        this.open4();
+      } 
     },
-    psw1Blur:function(){
-      if(this.password !== this.password1){
-        this.password1Show = true;
-      }else{
-        this.password1Show = false;
-      }
+    psw1Blur: function() {
+      if (this.password !== this.password1) {
+        this.errorMsg = "两次密码不匹配";
+        this.open4();
+      } 
     },
     // 获取短信验证码
-    sendSms:function(){
+    sendSms: function() {
       var that = this;
-      if((/^1[34578]\d{9}$/.test(this.phone))){
+      if (/^1[34578]\d{9}$/.test(this.phone)) {
         let data = {
-          'phone':that.phone
-        }
-        that.$request.post('user/exitByPhone',data,that.token).then(function(res) {
-          if(res.data.success == '校验成功'){
-            that.sendAuthCode = false;
-            that.auth_time = 60;
-            var auth_timetimer =  setInterval(()=>{
+          phone: that.phone
+        };
+        that.$request
+          .post("user/exitByPhone", data, that.token)
+          .then(function(res) {
+            if (res.data.success == "校验成功") {
+              that.sendAuthCode = false;
+              that.auth_time = 60;
+              var auth_timetimer = setInterval(() => {
                 that.auth_time--;
-                if(that.auth_time<=0){
-                    that.sendAuthCode = true;
-                    clearInterval(auth_timetimer);
+                if (that.auth_time <= 0) {
+                  that.sendAuthCode = true;
+                  clearInterval(auth_timetimer);
                 }
-            }, 1000);
-            that.$request.post('user/sendSms',data,that.token).then(function(res) {
-              console.log(res)
-              if(res.data.success == '发送成功'){
-                that.msgColor = true
-                that.codeMsg = '短信发送成功';
-              }
-            }).catch(function(error) {
-                that.msgColor = false
-                that.codeMsg = '短信发送失败';
-            });
-          }else{
-            that.codeMsg = '手机号未注册';
-          }
-        }); 
-      }else{
-        that.phoneShow = true;
+              }, 1000);
+              that.$request
+                .post("user/sendSms", data, that.token)
+                .then(function(res) {
+                  console.log(res);
+                  if (res.data.success == "发送成功") {
+                    that.successMsg = "短信发送成功";
+                    that.open1();
+                  }
+                })
+                .catch(function(error) {
+                  that.errorMsg = "短信发送失败";
+                  that.open4();
+                });
+            } else {
+              that.errorMsg = "手机号未注册";
+              that.open4();
+            }
+          });
+      } else {
+        that.errorMsg = "手机号码不正确";
+        that.open4();
       }
     },
     // 忘记密码修改
     forgrtPsw: function() {
       var that = this;
-      if(!(/^1[34578]\d{9}$/.test(this.phone))){
-        this.phoneShow = true;
+      if (!/^1[34578]\d{9}$/.test(that.phone)) {
+        that.errorMsg = "手机号码不正确";
+        that.open4();
         return;
-      }else {
-        this.phoneShow = false
+      } 
+      if(that.code == ""){
+        that.errorMsg = "请输入验证码";
+        that.open4();
+        return;
       }
-      if(!(/^(?=.*[a-z])(?=.*\d)[^]{8,20}$/.test(this.password))){
-        this.passwordShow = true;
+      if (!/^(?=.*[a-z])(?=.*\d)[^]{8,20}$/.test(that.password)) {
+        that.errorMsg = "密码只能含有字母和数字，长度在8-20之间";
+        that.open4();
         return;
-      }else {
-        this.passwordShow = false;
-      }
-      if(this.password !== this.password1){
-        this.password1Show = true;
+      } 
+      if (this.password !== this.password1) {
+        that.errorMsg = "两次密码不匹配";
+        that.open4();
         return;
-      }else{
-        this.password1Show = false;
       }
       var data = {
         phone: that.phone,
         password: that.password,
         code: that.code
       };
-      that.btnDis = true
-      that.$request.post("user/forgotPassword",data,that.token).then(function(res) {
-        if(res.data.success == "密码重置成功") {
-          that.SubmitShow = true;
-          that.SubmitMsg = '密码重置成功';
-          that.tipColor = true;
-          setTimeout(function(){
-            that.$router.push({
-              path:'/login',
-            })
-          },2000)
-          
-        }else {
-          that.SubmitShow = true;
-          that.SubmitMsg = '密码重置失败';
-          that.tipColor = false;
-          that.btnDis = false
-        }
-      }).catch(function(error) {
-        that.btnDis = false
-        console.log(error);
+      that.btnDis = true;
+      that.$request
+        .post("user/forgotPassword", data, that.token)
+        .then(function(res) {
+          if (res.data.success == "密码重置成功") {
+            that.successMsg = "密码重置成功";
+            that.open1();
+            setTimeout(function() {
+              that.$router.push({
+                path: "/login"
+              });
+            }, 2000);
+          } else {
+            that.errorMsg = "密码重置失败";
+            that.open4(); 
+            that.btnDis = false;
+          }
+        })
+        .catch(function(error) {
+          that.btnDis = false;
+          console.log(error);
+        });
+    },
+    open1() {
+      this.$notify({
+        title: "成功",
+        message: this.successMsg,
+        type: "success"
+      });
+    },
+
+    open2() {
+      this.$notify({
+        title: "警告",
+        message: this.warningMsg,
+        type: "warning"
+      });
+    },
+
+    open3() {
+      this.$notify.info({
+        title: "消息",
+        message: this.Message
+      });
+    },
+
+    open4() {
+      this.$notify.error({
+        title: "错误",
+        message: this.errorMsg,
+        type: "error"
       });
     }
-  },
-  
-   
+
+  }
+
 };
 </script>
 
@@ -292,7 +358,8 @@ export default {
   height: 42px;
   overflow: hidden;
 }
-.u-ipt,.u-ipt .ipt_wrap {
+.u-ipt,
+.u-ipt .ipt_wrap {
   display: inline-block;
   position: relative;
 }
@@ -316,12 +383,12 @@ export default {
   background: #fff;
 }
 .u-ipt .ipt_tip {
-    font-size: 12px;
-    line-height: 22px;
-    min-height: 24px;
-    text-align: left;
-    word-wrap: break-word;
-    word-break: break-all;
+  font-size: 12px;
+  line-height: 22px;
+  min-height: 24px;
+  text-align: left;
+  word-wrap: break-word;
+  word-break: break-all;
 }
 .tipSuccess {
   color: rgb(45, 168, 14);
@@ -329,20 +396,20 @@ export default {
 .tipFailed {
   color: rgb(224, 43, 11);
 }
-.u-ipt .ipt_extend>i {
-    margin-right: 12px;
-    cursor: pointer;
+.u-ipt .ipt_extend > i {
+  margin-right: 12px;
+  cursor: pointer;
 }
 
 .i-icon-delete {
-    background-position: 0 -55px;
-    width: 20px;
-    height: 20px;
+  background-position: 0 -55px;
+  width: 20px;
+  height: 20px;
 }
 .i-icon {
-    display: inline-block;
-    background-repeat: no-repeat;
-    background-image: url(/naq/findPassword/pub/img/sprite.adfc74b….png);
+  display: inline-block;
+  background-repeat: no-repeat;
+  background-image: url(/naq/findPassword/pub/img/sprite.adfc74b….png);
 }
 .u-btn-sms {
   position: initial;
@@ -353,7 +420,7 @@ export default {
   top: 0;
   right: 0;
 }
-.u-btn-sms>button{
+.u-btn-sms > button {
   border-radius: 2px;
   text-align: center;
   cursor: pointer;
@@ -366,7 +433,7 @@ export default {
   line-height: 42px;
 }
 
-.u-btn>button {
+.u-btn > button {
   width: 340px;
   height: 44px;
   /* line-height: 44px; */
@@ -386,46 +453,46 @@ export default {
   background-color: #0c0aa3;
 } */
 .m-verify .m-ipt {
-    display: block;
-    position: relative;
-    width: 340px;
-    margin-top: 8px;
+  display: block;
+  position: relative;
+  width: 340px;
+  margin-top: 8px;
 }
 .m-ipt.ipt-sms .ipt_wrap {
-    margin-right: 112px;
+  margin-right: 112px;
 }
 .m-ipt .u-ipt .ipt_input_small {
-    width: 228px;
+  width: 228px;
 }
 .m-ipt .u-ipt .ipt_input {
-    font-size: 12px;
-    line-height: 1.8em;
-    background-color: #fff;
+  font-size: 12px;
+  line-height: 1.8em;
+  background-color: #fff;
 }
 
 .u-ipt .ipt_input_small {
-    width: 212px;
+  width: 212px;
 }
 .u-ipt .ipt_input {
-    width: 228px;
-    height: 42px;
-    line-height: 42px;
-    border: 1px solid #ccc;
-    border-radius: 2px;
-    padding-left: 12px;
+  width: 228px;
+  height: 42px;
+  line-height: 42px;
+  border: 1px solid #ccc;
+  border-radius: 2px;
+  padding-left: 12px;
 }
 input {
-    width: 225px;
-    height: 30px;
-    padding-left: 0;
-    color: #333;
-    border: none;
-    font-size: 14px;
-    line-height: 30px;
-    -webkit-tap-highlight-color: rgba(255,255,255,0);
-    -webkit-appearance: none;
+  width: 225px;
+  height: 30px;
+  padding-left: 0;
+  color: #333;
+  border: none;
+  font-size: 14px;
+  line-height: 30px;
+  -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
+  -webkit-appearance: none;
 }
-.btnTip{
+.btnTip {
   line-height: 40px;
   text-align: center;
 }
